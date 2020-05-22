1731a1d48486136af3e2987db11b98df6a105ddb
Karen Lei <karenwinnielei@gmail.com>
2020-05-22 10:24:12 -0700

    complete cypress testing

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -6,8 +6,7 @@ const formSchema = yup.object().shape({
     .min(2, 'Name must be at least two characters long')
     .required('Name is a required field'),
     size: yup.string().required('Size is a required field'),
-    toppings: yup.boolean(true || false),
-    instructions: yup.string('')
+    instructions: yup.string()
 })
 
 export default formSchema
